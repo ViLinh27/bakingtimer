b5d2bf3fbf849c3af43bd38fec9638bf2fd8d549
Vi-Linh Nguyen <vilinh27@gmail.com>
2025-04-14 13:05:42 -0500

    Adding webPreferences from tutorial and activate for waiting for window

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 //controls app's event lifecycle
 const { app, BrowserWindow } = require('electron')
 
+// include the Node.js 'path' module at the top of your file
+const path = require('node:path')
+
 //creates/manages app window and lets us open in browser
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
 
   win.loadFile('index.html')
@@ -14,9 +20,16 @@ const createWindow = () => {
 //wiats for whenReady then creates our browser window
 app.whenReady().then(() => {
   createWindow()
+
+  //open window if none are open in macOS
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
 })
 
 //in case we're not on mac and need to close:
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
+
+
---
+++ preload.js
@@ -0,0 +1,11 @@
+//runs script before renderer process, access to both renderer globals (window andd coument a Node.js env)
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency])
+  }
+})
